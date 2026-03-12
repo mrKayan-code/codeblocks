6f5738d8e460c6eab404f67c76292d9ba88da2ca
mr_kayan <mrkayan999@gmail.com>
2026-03-13 00:40:50 +0700

    косметик

--- main.js
+++ main.js
@@ -15,20 +15,6 @@ const interpretator = new InterpretatorManager((msg) => {
             consoleOutput.appendChild(line);
         });
 
-// function getDragAfterElement (container, y) {
-//     const draggableElement = [...container.querySelectorAll('[class^="block"]:not(.dragging), .block:not(.dragging)')];
-//     return draggableElement.reduce((closest, child) => {
-//         const box = child.getBoundingClientRect();
-//         const offset = y - box.top - box.height / 2;
-
-//         if (offset < 0 && offset > closest.offset) {
-//             return {offset: offset, element: child};
-//         } else {
-//             return closest;
-//         }
-//     }, {offset: Number.NEGATIVE_INFINITY}).element;
-// }
-
 
 const originalBlocks = document.querySelectorAll('#palette [class^="block"]');
 originalBlocks.forEach(block => {
